5e97077bfecb5338978ab6818b6b5761ba35acad
Cleiton080 <cleitonpessoa53@hotmail.com>
2019-04-13 12:45:00 -0300

    Alterado a estrutura do arquivo blade employeedevelopment

--- treinamento/resources/views/admin/employeedevelopment.blade.php
+++ treinamento/resources/views/admin/employeedevelopment.blade.php
@@ -1,25 +1,29 @@
-@extends('templates.table')
-@extends('admin.administrator.administratormenu')
 @extends('templates.struct')
 
-@section('title')   
-Funcionário 
-@endsection
-<div style="margin-top:-10px;">Lazal</div>
+@section('title', 'Funcionário')
+
 @section('css')
   <link rel="stylesheet" type="text/css" href="{{ asset('css/menu.css') }}">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 @endsection
 
 @section('body')
-    @section('tabletitle')
-        <th scope="col">Nome</th>
-        <th scope="col">Número de acertos</th>
-        <th scope="col">Número de erros</th>
-        <th scope="col">Área</th>
-        <th scope="col">Status</th>
-    @endsection
-    @section('body')
+
+    @component('admin.administrator.administratormenu') @endcomponent
+
+    <div class="text-center">
+        <button type="button" class="cadast btn btn-outline-success btn-lg" data-toggle="modal" data-target="#register">Cadastrar</button>
+    </div>
+
+    @component('admin.administrator.table')
+        @slot('head')
+            <th scope="col">Nome</th>
+            <th scope="col">Número de acertos</th>
+            <th scope="col">Número de erros</th>
+            <th scope="col">Área</th>
+            <th scope="col">Status</th>
+        @endslot
         <td></td>
-    @endsection 
+    @endcomponent
+
 @endsection
