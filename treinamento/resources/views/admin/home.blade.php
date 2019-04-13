98d454cb29714fda7a00fcb53c38539a340da001
Cleiton080 <cleitonpessoa53@hotmail.com>
2019-04-13 12:47:19 -0300

    Alterado a estrutura do arquivo blade home

--- treinamento/resources/views/admin/home.blade.php
+++ treinamento/resources/views/admin/home.blade.php
@@ -1,14 +1,15 @@
-@extends('templates.table')
-@extends('admin.administrator.administratormenu')
 @extends('templates.struct')
 
-<div style="margin-top:-10px;">Lazal</div>
 @section('css')
   <link rel="stylesheet" type="text/css" href="{{ asset('css/menu.css') }}">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 @endsection
 
 
-@section('title')
-    Home
+@section('title', 'Home')
+
+@section('body')
+
+  @component('admin.administrator.administratormenu') @endcomponent
+
 @endsection
