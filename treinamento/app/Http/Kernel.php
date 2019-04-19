823c51cf94c17bf8163266b84a33bb801db5ffd8
Iago Andrei <iagoandreii@yahoo.com.br>
2019-04-19 07:09:47 -0300

    Implementação de middleware que verificar nível de acesso

--- treinamento/app/Http/Kernel.php
+++ treinamento/app/Http/Kernel.php
@@ -60,7 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'sessao' => \treinamento\Http\Middleware\SessaoUser::class
+        'sessao' => \treinamento\Http\Middleware\SessaoUser::class,
+        'nivelAcesso' => \treinamento\Http\Middleware\NivelAcesso::class
     ];
 
     /**
